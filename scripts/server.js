69209eab2c5adc21904598ddc002dcb41ea65f25
NicoGGomez <anelecarg@icloud.com>
2026-03-16 16:03:40 -0300

    cambio en server

--- scripts/server.js
+++ scripts/server.js
@@ -23,27 +23,7 @@ app.get("/formats", async (req, res) => {
         return res.status(400).json({error:"URL inválida"})
     }
 
-    let data = null
-
-    for (const instance of INSTANCES) {
-
-        try {
-
-            console.log("probando:", instance)
-
-            const response = await fetch(`${instance}/api/v1/streams/${videoId}`)
-
-            if (response.ok) {
-            console.log("funciona:", instance)
-            data = await response.json()
-            break
-            }
-
-        } catch (e) {
-            console.log("falló instancia:", instance)
-        }
-
-        }
+    const data = await getStreams(videoId)
 
     if(!data){
         return res.status(500).json({error:"No hay instancias disponibles"})
@@ -90,6 +70,43 @@ app.get("/downloadmp3", (req, res) => {
     stream.stdout.pipe(res)
 
 })
+
+async function getStreams(videoId){
+
+ for(const instance of INSTANCES){
+
+  try{
+
+   console.log("probando:", instance)
+
+   const response = await fetch(
+    `${instance}/api/v1/streams/${videoId}`
+   )
+
+   if(!response.ok) continue
+
+   const text = await response.text()
+
+   if(!text.startsWith("{")){
+     console.log("no es json:", instance)
+     continue
+   }
+
+   const data = JSON.parse(text)
+
+   console.log("funciona:", instance)
+
+   return data
+
+  }catch(e){
+   console.log("falló instancia:", instance)
+  }
+
+ }
+
+ return null
+}
+
 
 const PORT = process.env.PORT || 3000
 
